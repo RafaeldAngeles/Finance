554091079466593f12d644fbc2ada63c621d9e24
Rafael <rafael.angeles.toledano@gmail.com>
2024-11-13 11:39:43 -0300

    chore: add shandc - dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return (
-    <div className="items_center flex w-screen justify-center">
-      <h1 className="p-5 text-red-500">Hello wolrd</h1>
-    </div>
-  );
+  return <Button>Hello World</Button>;
 };
 
 export default Home;
